e18ab1d3f60d80aca78be6c71b5c9ca8b21261f2
Alexandria Strider <alexandria.strider@yahoo.com>
2018-02-06 18:52:14 -0500

    java practice

---
+++ exercises/greeter.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Greeter!</title>
+</head>
+ <body>
+
+  <script type="text/javascript" src="greeting.js"></script>
+
+
+  var name = prompt("Hi there! What's your name?");
+  var output = document.querySelector('#greeting');
+output.textContent = "Thanks for visiting, " + name + ".</p>";
+</body>
