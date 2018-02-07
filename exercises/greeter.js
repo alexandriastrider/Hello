9aef86927a9714273cfcd2c9fb26bbf2c49ea6a9
Alexandria Strider <alexandria.strider@yahoo.com>
2018-02-06 19:19:56 -0500

    trying to fix things

--- exercises/greeter.js
+++ exercises/greeter.js
@@ -14,5 +14,7 @@
   var name = prompt("Hi there! What's your name?");
   var output = document.querySelector('#greeting');
 output.textContent = "Thanks for visiting, " + name + ".</p>";
+
+
 </body>
 </html>
