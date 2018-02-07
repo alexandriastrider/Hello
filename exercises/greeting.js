94eaf3a8a3210122a5c1665e5d144436c98f1b88
Alexandria Strider <alexandria.strider@yahoo.com>
2018-02-06 20:31:32 -0500

    trying thingd out

--- exercises/greeting.js
+++ exercises/greeting.js
@@ -7,29 +7,15 @@
   <title>Greeter!</title>
 </head>
 <body>
-
-
-  var name = prompt("Hi there! What's your name?");
-  var output = document.querySelector('#greeting');
-output.textContent = "Thanks for visiting, " + name + ".</p>";
-
-var firstName = prompt("Hi there! What's your first name?");
-var lastName = prompt("What's your last name?");
-var output = document.querySelector('#greeting');
-
-if(firstName && lastName){
-   output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
-} else {
-   output.innerHTML = "<p>Please tell us your first and last names!</p>";
-}
-
-var firstName = prompt("Hi there! What's your first name?") || "Visitor";
-var lastName = prompt("What's your last name?") || "McDefaultson";
-var output = document.querySelector('#greeting');
-
-output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
-
 <script type="text/javascript" src="greeting.js"></script>
+
+var name = prompt("Hi there! What's your name?");
+var output = document.querySelector('#greeting');
+output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
+
+var greeting = prompt("Hello there, how are you today?");
+
+
 
 </body>
 </html>
