7dbbdec5fb1aa99ac00682a3375892073075b779
Alexandria Strider <alexandria.strider@yahoo.com>
2018-02-10 13:45:06 -0500

    trying to get greeting.js to work

--- exercises/greeting.js
+++ exercises/greeting.js
@@ -1,9 +1,6 @@
-
-<script type="text/javascript" src="greeting.js"></script>
-
-
 var name = prompt("Hi there! What's your name?");
 var output = document.querySelector('#greeting');
 output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
-
-var greeting = prompt("Hello there, how are you today?");
+var name = prompt("Hi there! What's your name?");
+var output = document.querySelector('#greeting');
+output.textContent = "Thanks for visiting, " + name + ".";
