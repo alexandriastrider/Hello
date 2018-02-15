e16cea513f0ef4fe56e95c8d19e00b30f9984c55
Alexandria Strider <alexandria.strider@yahoo.com>
2018-02-15 12:42:11 -0500

    updating jQuery

--- projects/sandbox/sandbox.js
+++ projects/sandbox/sandbox.js
@@ -7,3 +7,33 @@ $('h1+p').css('color','blue');
 $('#secret').css('display','none');
 $('#change-me').text('');
 $('#change-me').html('<p></p>');
+// inlined
+$("#change-me").css({
+    "width": "80%",
+    "margin-left": "auto",
+    "margin-right": "auto"
+});
+
+// separated
+var options = {
+    "width": "80%",
+    "margin-left": "auto",
+    "margin-right": "auto"
+};
+
+$("#change-me").css( options );
+// inlined
+$("#change-me").animate({
+    "font-size": "300%",
+    "margin-left": "200px",
+    "padding": "50px"
+});
+
+// separated
+var options = {
+    "font-size": "300%",
+    "margin-left": "200px",
+    "padding": "50px"
+};
+
+$("#change-me").animate( options );
