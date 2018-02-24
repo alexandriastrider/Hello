554ea8c4d730eecc9364288b03f188ceb6808683
Alexandria Strider <alexandria.strider@yahoo.com>
2018-02-24 13:15:27 -0500

    adding arrays

--- contacts/contacts.js
+++ contacts/contacts.js
@@ -1,2 +1,11 @@
 <ul id = "contact-array"></ul>
- let contactArray = ['']
+ let contactArray = ['Allie Strider', 423-555-5555, 'alexandria.strider@yahoo.com']
+ contactArray[3] = 'A new item'
+ contactArray[4] = 'alexandria.strider@yahoo.com'
+
+ function addContent(array){
+   console.log(array)
+   $("#contact-array").append("<li>" + array + "</li>");
+ }
+ contactArray.forEach(addContent)
+ 
